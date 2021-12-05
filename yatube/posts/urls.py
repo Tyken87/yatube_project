792e31fa7786c738993c4f159ac3ff0bc529136c
Данияр <dan-1987@mail.ru>
2021-12-05 14:36:32 +0300

    сделал разбивку шаблонов на header, footer и base

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index),
     path('group/', views.group_posts),
     path('group/<slug:slug>/', views.group_posts_slug),
-
+    path('group_list/', views.group_list),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,16 @@
+from django import template
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Главная страница")
+    template = 'posts/index.html'
+    return render(request, template)
+
+def group_list(request):
+    template = 'posts/group_list.html'
+    return render(request, template)
 
 def group_posts(request):
     return HttpResponse("Тут будем выкладывать посты, ожидайте...")
