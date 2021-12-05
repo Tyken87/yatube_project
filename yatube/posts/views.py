af3edefbda598733c8955489eb0d2df7d67a9fb4
Данияр <dan-1987@mail.ru>
2021-12-05 23:00:28 +0300

    начал работать с базами данных

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,5 @@
-from django import template
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import loader
 
 # Create your views here.
 def index(request):
