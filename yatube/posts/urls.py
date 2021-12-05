31e037a0184e55749087e3f8916ad2eef1f82a8d
Данияр <dan-1987@mail.ru>
2021-12-05 18:08:17 +0300

    Освоил name и namespace

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'posts'
 urlpatterns = [
-    path('', views.index),
-    path('group/', views.group_posts),
-    path('group/<slug:slug>/', views.group_posts_slug),
-    path('group_list/', views.group_list),
+    path('', views.index, name='index'),
+    path('group/', views.group_posts, name='group_posts'),
+    path('group/<slug:slug>/', views.group_posts_slug, name='group_posts_slug'),
+    path('group_list/', views.group_list, name='group_list'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,14 +6,25 @@ from django.template import loader
 # Create your views here.
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    text = "Это главная страница проекта Yatube"
+    context = {
+        'title': "Главная страница",
+        'text': text, 
+    }
+    return render(request, template, context)
 
 def group_list(request):
     template = 'posts/group_list.html'
     return render(request, template)
 
 def group_posts(request):
-    return HttpResponse("Тут будем выкладывать посты, ожидайте...")
+    template = 'posts/index.html'
+    text = "Здесь будет информация о группах проекта Yatube"
+    context = {
+        'title': 'Информация о группах',
+        'text' : text,
+    }
+    return render(request, template, context)
 
 def group_posts_slug(request, slug):
     return HttpResponse("Да выложим мы посты, выложим!!!")
