5f4d56452c72b82bcb5f4e69411cd6626bec5e2b
Данияр Тукенов <dan-1987@mail.ru>
2021-12-06 12:38:13 +0300

    вообще запутался

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.deletion import CASCADE
+from django.db.models.fields.related import ForeignKey
 
 User = get_user_model()
+
+class Group(models.Model):
+    def __str__(self):
+        return self.title
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
 
 class Post(models.Model):
     text = models.TextField()
@@ -10,6 +20,15 @@ class Post(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='posts'
+        )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE
+        
     )
+
+
 
 # Create your models here.
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import get_object_or_404, render
+from .models import Post, Group
 
 # Create your views here.
 def index(request):
@@ -15,14 +15,31 @@ def group_list(request):
     template = 'posts/group_list.html'
     return render(request, template)
 
-def group_posts(request):
-    template = 'posts/index.html'
-    text = "Здесь будет информация о группах проекта Yatube"
+def group_posts(request, slug):
+    # Функция get_object_or_404 получает по заданным критериям объект 
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': 'Информация о группах',
-        'text' : text,
+        'group': group,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context) 
+
+#def group_posts(request):
+#    template = 'posts/index.html'
+#    text = "Здесь будет информация о группах проекта Yatube"
+#    context = {
+#        'title': 'Информация о группах',
+#        'text' : text,
+#    }
+#    return render(request, template, context)
 
 def group_posts_slug(request, slug):
     return HttpResponse("Да выложим мы посты, выложим!!!")
